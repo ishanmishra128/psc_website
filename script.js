60d2fd0098059c97822c8995ab2b1c4e12208ae0
Celeste <marcosgarcia@wisc.edu>
2026-03-19 00:04:06 -0500

    Added contact page

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#contactForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent refresh
+
+    const name = form.name.value;
+    const email = form.email.value;
+    const subject = form.subject.value;
+    const message = form.message.value;
+
+    console.log({ name, email, subject, message });
+
+    alert("Message submitted!");
+
+    form.reset();
+  });
+});
